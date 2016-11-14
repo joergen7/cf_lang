983d47ff3870393fb3c3238c26a7a224133aff38
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-11-14 12:48:20 +0100

    Lisence boiler plate added.

--- src/cf_tcp_srv.erl
+++ src/cf_tcp_srv.erl
@@ -1,4 +1,25 @@
+%% -*- erlang -*-
+%%
+%% Cuneiform: A Functional Language for Large Scale Scientific Data Analysis
+%%
+%% Copyright 2016 Jörgen Brandt, Marc Bux, and Ulf Leser
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%    http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+%% @author Jörgen Brandt <brandjoe@hu-berlin.de>
+
 -module( cf_tcp_srv ).
+-author( "Jörgen Brandt <brandjoe@hu-berlin.de>" ).
 
 -define( TCP_PORT, 17489 ).
 -define( LISTEN_OPT, [binary, {packet, 4}, {reuseaddr, true}, {active, true}] ).
